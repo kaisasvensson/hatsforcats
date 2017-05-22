c10f27910e3f665298de6828c3fed8d7f33ff01b
kaisasvensson <kaisa.svensson@hotmail.com>
2017-05-22 12:49:15 +0200

    added project

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(450);
+
+       e.preventDefault();
+    });
+
+
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(450);
+ 
+        e.preventDefault();
+    });
+});
